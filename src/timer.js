e35679a7da17cae710e27c32f37e430b58598e50
rendfall <rendfall@gmail.com>
2017-08-31 00:53:12 +0200

    Fix refresh counter + clean up

--- src/timer.js
+++ src/timer.js
@@ -1,4 +1,4 @@
-const TICK_INTERVAL = 750;
+const TICK_INTERVAL = 250;
 
 function lpad(n) {
     return (String(n).length === 1) ? `0${n}` : `${n}`;
@@ -9,11 +9,10 @@ function tick(startFrom) {
     let diff = d - startFrom;
     let minutes = Math.floor(diff / 1000 / 60);
     let seconds = Math.floor(diff / 1000) - minutes * 60;
-
     self.postMessage(`${lpad(minutes)}:${lpad(seconds)}`);
 }
 
-function onMessage(message) {
+function onStart(message) {
     let now = Date.now();
     self.setInterval(() => tick(now), TICK_INTERVAL);
 }
@@ -21,7 +20,8 @@ function onMessage(message) {
 self.addEventListener('message', (evt) => {
     switch (evt.data) {
         case 'start':
-            return onMessage.call(onMessage, evt.data);
+            return onStart.call(onStart, evt.data);
+
         default:
             return;
     }
